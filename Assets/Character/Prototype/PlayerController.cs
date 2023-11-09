21773f0459caddde825a9d984d219161655a5a77
hujillian <hujillian123@gmail.com>
2023-11-08 22:36:58 -0500

    Limit player to 1 jump

--- Assets/Character/Prototype/PlayerController.cs
+++ Assets/Character/Prototype/PlayerController.cs
@@ -12,6 +12,9 @@ public class PlayerController : MonoBehaviour
     private float inputX;
     private Rigidbody2D rb;
 
+    private bool jumping = false;   // check whether the player has jumped
+    private int jumpCount = 0;      // counter for double jump
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,9 +35,12 @@ public class PlayerController : MonoBehaviour
             inputX = moveSpeed;
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && (!jumping))
         {
             rb.AddForce(Vector2.up* jumpForce, ForceMode2D.Impulse);
+
+            jumpCount += 1;
+            jumping = checkJump(1); // check how many jumps have been done
         }
 
         //Movement
@@ -43,5 +49,28 @@ public class PlayerController : MonoBehaviour
         transform.Translate(movementX);
         //transform.position += new Vector3(inputX, 0f, 0f);
         inputX = 0f;
+    }
+
+    // check if the player has jumped once
+    // I have this as a function in case we want to include a double jump
+    private bool checkJump(int maxJump)
+    {
+        if (jumpCount >= maxJump)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // check if the player has collided with the floor
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if collided, allow the player to jump again
+        if (collision.gameObject.tag == "Floor")
+        {
+            // reset jump variables
+            jumping = false;
+            jumpCount = 0;
+        }
     }
 }
