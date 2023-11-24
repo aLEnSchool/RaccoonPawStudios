5870c68e07dc4b141c7432fe4e697e4b527d74df
aLEnSchool <alexnita@cmail.carleton.ca>
2023-11-24 13:35:43 -0500

    Interactions Done

--- Assets/Objects/InformationController.cs
+++ Assets/Objects/InformationController.cs
@@ -12,21 +12,51 @@ public class InformationController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
+
         inRange = false; interact = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inRange)
+        {
+            sprite.color = Color.red;
+        }
+        else
+        {
+            sprite.color = Color.white;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && inRange) 
+        {
+            if (interact)
+            {
+                Debug.Log("Player Interacting");
+                sprite.color = Color.cyan;
+            }
+            else
+            {
+                interact = true;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        inRange = true;
+        if (collision.gameObject.name == "Player") {
+            Debug.Log("Player is in");
+            inRange = true;
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        inRange = false;
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Player Out");
+            inRange = false;
+            interact = false;
+        }
     }
 }
