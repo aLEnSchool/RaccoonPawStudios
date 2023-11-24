bf024b62b1e330f067e3b95e9ffe86fd5c118422
aLEnSchool <alexnita@cmail.carleton.ca>
2023-11-24 12:10:44 -0500

    testing

--- Assets/Objects/ObjectController.cs
+++ Assets/Objects/ObjectController.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEditor.UIElements;
+
 public class ObjectController : MonoBehaviour
 {
+    private bool popUpAppear;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        popUpAppear = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (popUpAppear)
+        {
+            Debug.Log("HAHAHAHAHA");
+        }
+        else
+        {
+            Debug.Log("Booo");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            //Debug.Log("Player Enters");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Popup Appears");
+                if (popUpAppear)
+                {
+                    popUpAppear = false;
+                }
+                else
+                {
+                    popUpAppear = true;
+                }
+            }
+        }
     }
 }
