9728106774400b6aab6cda61e4c4758ddd0b9cf2
aLEnSchool <146203376+aLEnSchool@users.noreply.github.com>
2024-01-11 10:50:59 -0500

    Grumpy Man

--- Assets/Objects/CaseFile1/Characters/#3CustomerController.cs
+++ Assets/Objects/CaseFile1/Characters/#3CustomerController.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class #3CustomerController : MonoBehaviour
+public class Customer3Controller : MonoBehaviour
 {
+    private string[] voiceLines;
+    private int voiceLineIndex;
+
+    private bool inRange;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inRange = false;
+
+        InitializeVoiceLines();
+        voiceLineIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E)) 
+            {
+                Debug.Log(voiceLines[voiceLineIndex]);
+                voiceLineIndex++;
+
+                if (voiceLineIndex >= 2)
+                {
+                    voiceLineIndex = 2;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inRange = false;
+        Debug.Log(voiceLines[3]);
+    }
+
+    private void InitializeVoiceLines()
+    {
+        voiceLines = new string[4] {"I WANT FOOD!","GET ME FOOD","FOOOOOOOD!","food..." };
     }
 }
