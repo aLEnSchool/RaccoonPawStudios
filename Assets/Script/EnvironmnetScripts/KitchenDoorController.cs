ae20d836e77bb6e9eac964f74dcf3a790413c07c
aLEnSchool <146203376+aLEnSchool@users.noreply.github.com>
2024-01-13 14:05:15 -0500

    Restricted Kitchen Door

--- Assets/Script/EnvironmnetScripts/KitchenDoorController.cs
+++ Assets/Script/EnvironmnetScripts/KitchenDoorController.cs
@@ -5,28 +5,62 @@ using UnityEngine;
 
 public class KitchenDoorController : MonoBehaviour
 {
-    public WaitressController sadie;
+    public WaitressController Sadie;
 
+    private bool inRange;
+
+    private string voiceLine;
+
+    //Dialog Variables
     public GameObject dialogBox;
     public TMP_Text dialogOutput;
-    private string voiceLine;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        inRange = false;
+
+        dialogBox.SetActive(false);
         voiceLine = "Can't come in here, it's restricted";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (sadie.cookBusy)
+        if (Sadie.cookBusy)
         {
             //TP TO KITCHEN
         }
         else
         {
+            if (inRange)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    dialogBox.SetActive(true);
+                    dialogOutput.text = voiceLine;
+                }
+            }
+        }
+    }
 
+    /*-- Trigger Events --*/
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inRange = false;
+        
+            dialogBox.SetActive(false);
         }
     }
 }
