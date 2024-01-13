0257dd574717eb1fa7ca0ed96b053060fd745959
hujillian <hujillian123@gmail.com>
2024-01-13 11:31:18 -0500

    Exit door code (not working yet)

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Came : MonoBehaviour
 {
     public Transform target;
     public float yPos=0f;
+
+    [SerializeField] private ExitDoor exitDoor;
     private void Start()
     {
         yPos = 0f;
@@ -14,5 +17,12 @@ public class Came : MonoBehaviour
     void FixedUpdate()
     {
         this.transform.position = new Vector3(target.position.x, yPos, this.transform.position.z);
+
+        // if the player exits the door at the end
+        //if (exitDoor.exitRoom)
+        //
+            // change camera position to the new room
+        //    yPos = -15.5f;
+        //}
     }
 }
