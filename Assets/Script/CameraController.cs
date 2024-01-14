afe4984187633a02ef4821895290337d40894400
hujillian <hujillian123@gmail.com>
2024-01-13 22:36:00 -0500

    Player changes location when they interact with exit door

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -8,7 +8,8 @@ public class Came : MonoBehaviour
     public Transform target;
     public float yPos=0f;
 
-    [SerializeField] private ExitDoor exitDoor;
+    [SerializeField] private ExitDoor exitDoor; // to access exit door script
+    [SerializeField] private GameObject player; // to access player location
 
     private void Start()
     {
@@ -19,11 +20,13 @@ public class Came : MonoBehaviour
     {
         this.transform.position = new Vector3(target.position.x, yPos, this.transform.position.z);
 
+
         // if the player exits the door at the end
         if (exitDoor.exitRoom) { 
-        
             // change camera position to the new room
             yPos = -15.5f;
+            // change the player position to be in the next room
+            player.transform.position = new Vector3(-5.935f, -18f, -0.52f);
         }
     }
 }
