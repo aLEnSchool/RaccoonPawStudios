0beff7026f0b61b6f4bc0086ad76a8463d034432
aLEnSchool <146203376+aLEnSchool@users.noreply.github.com>
2024-03-07 20:33:53 -0500

    SceneManagement

---
+++ Assets/Script/ToCaseFileController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class ToCaseController : MonoBehaviour
+{
+    private bool inRange;
+    public GameObject playertemp;
+
+    public GameObject popUp;
+
+    private void Awake()
+    {
+        playertemp = GameObject.FindGameObjectWithTag("DontDestroy");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inRange = false;
+        popUp.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                popUp.SetActive(true);
+
+                //playerDataController.nextScene();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inRange = false;
+        }
+    }
+
+    public void toNextScene()
+    {
+        Debug.Log("Go to next Scene");
+        playertemp.GetComponent<PlayerDataController>().nextScene();
+    }
+
+}
