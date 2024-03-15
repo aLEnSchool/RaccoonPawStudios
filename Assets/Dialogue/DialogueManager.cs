dfffebd9ff6cf3a1fd3b2f95a09aed5d51d91aa9
hujillian <hujillian123@gmail.com>
2024-03-15 13:46:10 -0400

    Squashed commit of the following:
    commit 72db7e0d133ab3a72bf08247a572acb6f8ee1664
    Author: shortCV <cversace18@gmail.com>
    Date:   Fri Mar 15 12:25:02 2024 -0400
    
        tewsting
    
    commit 9926a28d6a6e8bffcdd23fc76833ce4fdc2a32fa
    Author: shortCV <cversace18@gmail.com>
    Date:   Thu Mar 14 18:37:07 2024 -0400
    
        fijhodrghoid
    
    commit 3033e299318d834f2de9f5dcd114247abc1e3bc2
    Author: shortCV <cversace18@gmail.com>
    Date:   Thu Mar 14 18:36:42 2024 -0400
    
        we got the basics!

---
+++ Assets/Dialogue/DialogueManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Ink.Runtime;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject dialoguePanel;
+    
+    private static DialogueManager instance;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    private Story currentStory;
+    public bool dialogueIsPlaying { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Found more than one Dialogue Manager in the scene");
+        }
+        instance = this;
+    }
+
+    public static DialogueManager GetInstance()
+    {
+        return instance;
+    }
+    private void Start()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!dialogueIsPlaying)
+        {
+            PlayerController.instance.canMove = true;
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ContinueStory();
+        }
+    }
+
+    public void EnterDialogueMode(TextAsset inkJSON)
+    {
+        currentStory = new Story(inkJSON.text);
+        dialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+
+        ContinueStory();
+    }
+
+    private IEnumerator ExitDialogueMode()
+    {
+        yield return new WaitForSeconds(0.2f);
+       
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = "";
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+            PlayerController.instance.canMove = true;
+        }
+        else
+        {
+            StartCoroutine(ExitDialogueMode());
+        }
+    }
+}
+
+
