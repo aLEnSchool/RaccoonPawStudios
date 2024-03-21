c59fe0e815153cb5866552593f295d40fc58e106
aLEnmeister2 <alen.studios2@gmail.com>
2024-03-20 22:37:12 -0400

    Adding to level1

---
+++ Assets/Objects/CaseFile1/INKFiles/ClayeDialog1.ink
@@ -0,0 +1,3 @@
+Hey Sweetie.
+Sweetie I've been waiting for so long.
+Be a dear and get me a drink.
---
+++ Assets/Script/CharacterScripts/FrannieDialogTrigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrannieDialogueTrigger : MonoBehaviour
+{
+    [Header("Ink Files")]
+    [SerializeField] private TextAsset dialog1;
+
+    private bool playerInRange;
+
+    private void Awake()
+    {
+        playerInRange = false;
+    }
+
+    private void Update()
+    {
+        if (playerInRange && !DialogueManager.GetInstance().dialogueIsPlaying)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DialogueManager.GetInstance().EnterDialogueMode(dialog1);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+}
