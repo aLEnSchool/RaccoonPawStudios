8c08f338348f81115cc8307a3010ed076d7c7087
hujillian <hujillian123@gmail.com>
2024-03-22 20:18:43 -0400

    sadie dialogue trigger

---
+++ Assets/Script/CharacterScripts/SadieDialogueTrigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SadieDialogueTrigger : MonoBehaviour
+{
+    [Header("Ink Files")]
+    [SerializeField] private TextAsset dialogFile1;
+
+    private bool playerInRange;
+
+    private void Awake()
+    {
+        playerInRange = false;
+    }
+
+    private void Update()
+    {
+        if (playerInRange && !DialogueManager.GetInstance().dialogueIsPlaying)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DialogueManager.GetInstance().EnterDialogueMode(dialogFile1);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+}
