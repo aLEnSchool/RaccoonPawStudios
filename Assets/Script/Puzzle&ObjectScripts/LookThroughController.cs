bf848dd17efbf9c6ac36ad7e927c2cf8288ec151
aLEnSchool <alexnita@cmail.carleton.ca>
2024-03-30 21:28:00 -0400

    LookThroughController

--- Assets/Script/Puzzle&ObjectScripts/LookThroughController.cs
+++ Assets/Script/Puzzle&ObjectScripts/LookThroughController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class LookThroughController : MonoBehaviour
 {
-    private bool inRange;
+    public bool inRange;
 
     public GameObject LookThrough;
 
     // Start is called before the first frame update
     void Start()
     {
+        LookThrough.SetActive(false);
         inRange = false;
     }
 
@@ -19,21 +20,30 @@ public class LookThroughController : MonoBehaviour
     {
         if (inRange)
         {
-            LookThrough.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E)) { 
+                LookThrough.SetActive(true);
+            }
         }
         else
         {
             LookThrough.SetActive(false);
         }
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        inRange = true;
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Entered");
+            inRange = true;
+        }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        inRange = false;
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Exit");
+            inRange = false;
+        }
     }
 }
