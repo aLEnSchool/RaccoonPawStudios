a2b4b73d90a89c705bb696f4608a56892c1b6eed
aLEnSchool <alexnita@cmail.carleton.ca>
2024-03-30 20:41:17 -0400

    LookThrough

--- Assets/Script/Puzzle&ObjectScripts/LookThroughController.cs
+++ Assets/Script/Puzzle&ObjectScripts/LookThroughController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class LookThroughController : MonoBehaviour
 {
+    private bool inRange;
+
+    public GameObject LookThrough;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inRange = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inRange)
+        {
+            LookThrough.SetActive(true);
+        }
+        else
+        {
+            LookThrough.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        inRange = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        inRange = false;
     }
 }
