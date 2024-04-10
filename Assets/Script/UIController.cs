10d5439c9c51e0f0a06ed628bab6a2d66672ca4b
aLEnSchool <alexnita@cmail.carleton.ca>
2024-04-10 01:11:12 -0400

    Menu

--- Assets/Script/UIController.cs
+++ Assets/Script/UIController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIController : MonoBehaviour
@@ -10,9 +11,15 @@ public class UIController : MonoBehaviour
     [SerializeField] public GameObject playIcon;
     [SerializeField] public GameObject dontPlayIcon;
 
+    [Header("Menu Icons")]
+    [SerializeField] public GameObject menuIcon;
+    [SerializeField] private bool menuShow = false;
+
+    public GameObject playertemp;
     private void Awake()
     {
         instance = this;
+        playertemp = GameObject.FindGameObjectWithTag("DontDestroy");
     }
 
     // Start is called before the first frame update
@@ -20,11 +27,30 @@ public class UIController : MonoBehaviour
     {
         playIcon.SetActive(true);
         dontPlayIcon.SetActive(false);
+        menuIcon.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void toggleMenu()
     {
-        
+        if (menuShow)
+        {
+            menuIcon.SetActive(false);
+            menuShow = false;
+        }
+        else
+        {
+            menuIcon.SetActive(true);
+            menuShow = true;
+        }
+    }
+
+    public void goMainMenu()
+    {
+        playertemp.GetComponent<PlayerDataController>().mainMenu();
+    }
+
+    public void QuitGame()
+    {
+        playertemp.GetComponent<PlayerDataController>().quitGame();
     }
 }
